5b2c017f4d8a1efb38e5b3cb85a911d8ed565f4d
Gomina Maryam <gominamaryam@gmail.com>
2022-05-10 17:12:40 +0100

    initial commit

---
+++ src/lib/components/sections/ThoughtsHero.tsx
@@ -0,0 +1,54 @@
+import { Box, Text, Input, Button, Stack } from '@chakra-ui/react';
+
+function ThoughtsHero() {
+  return (
+    <Box
+      w="full"
+      h={['260px', '400px']}
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      backgroundColor="rgba(4, 12, 33, 0.7)"
+      backgroundImage="/heros.png"
+      backgroundBlendMode="overlay"
+      backgroundSize="cover"
+      px={['1rem', 16]}
+    >
+      <Text
+        color="white"
+        fontSize={['1.2rem', '52px']}
+        fontFamily="brEbold"
+        mb={3}
+      >
+        Lorem ipsum dolor sit amet
+      </Text>
+      <Text
+        color="white"
+        fontSize={['13px', '18px']}
+        w={{ base: 'full', md: '80%', lg: '60%' }}
+      >
+        Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui Lorem
+        cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat.
+      </Text>
+      <Stack
+        direction={['column', 'row']}
+        w={{ base: 'full', md: '80%', lg: '50%' }}
+        spacing={4}
+        mt="5"
+      >
+        <Input
+          rounded="5"
+          bg="gray.100"
+          placeholder="Enter your email address"
+          size="lg"
+        />
+
+        <Button bgColor="brand.100" rounded="5" color="white" h="3rem" px="14">
+          Subscribe
+        </Button>
+      </Stack>
+    </Box>
+  );
+}
+
+export default ThoughtsHero;
---
+++ src/lib/pages/home/Thought.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-unresolved */
+import Gist from 'lib/components/sections/Gist';
+import News from 'lib/components/sections/News';
+
+const Thought = () => (
+  <>
+    <Gist />
+    <News />
+  </>
+);
+export default Thought;
