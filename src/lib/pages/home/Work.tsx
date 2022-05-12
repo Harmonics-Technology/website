de5199c220751bd5b551e4f711f170599424d373
Lizzie <koladeelizabeth01@gmail.com>
2022-05-12 01:09:11 -0700

    commit work

--- src/lib/pages/home/Work.tsx
+++ src/lib/pages/home/Work.tsx
@@ -1,21 +1,25 @@
 import { Grid } from '@chakra-ui/react';
 import Work from '../../components/sections/Work';
+import PagesHero from '../../components/sections/PagesHero';
 
 const Works = () => (
-  <Grid
-    templateColumns="repeat(2,1fr)"
-    w="90%"
-    mx="auto"
-    gap={10}
-    p={['5rem', '2rem']}
-  >
-    <Work />
-    <Work />
-    <Work />
-    <Work />
-    <Work />
-    <Work />
+  <>
+    <PagesHero/>
+    <Grid
+      templateColumns="repeat(2,1fr)"
+      w="90%"
+      mx="auto"
+      gap={10}
+      p={['5rem', '2rem']}
+    >
+      <Work />
+      <Work />
+      <Work />
+      <Work />
+      <Work />
+      <Work />
   </Grid>
+  </>
 );
 
 export default Works;
