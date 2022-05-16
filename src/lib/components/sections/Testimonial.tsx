168b726ec42f8c89bda2b05e4ed8fd7d7388e5f2
Akinyemi Oluwabukunmi <braingrams40@gmail.com>
2022-05-16 21:49:45 +0100

    Added Few Changes

--- src/lib/components/sections/Testimonial.tsx
+++ src/lib/components/sections/Testimonial.tsx
@@ -2,7 +2,14 @@ import { Box, Text } from '@chakra-ui/react';
 import Sliders from './Sliders';
 
 const Testimonials = () => (
-  <Box bgColor="brand.100" h="780px" w="full" px={['1rem', 16]} py={10}>
+  <Box
+    bgColor="brand.100"
+    h="auto"
+    w="full"
+    px={['1rem', 16]}
+    pt="2rem"
+    pb="4rem"
+  >
     <Text
       color="white"
       fontSize={['1rem', '2.5rem']}
--- src/lib/styles/customTheme/fonts.ts
+++ src/lib/styles/customTheme/fonts.ts
@@ -1,8 +1,8 @@
 import type { DeepPartial, Theme } from '@chakra-ui/react';
 
 const fonts: DeepPartial<Theme['fonts']> = {
-  body: 'Lexend, sans-serif',
-  heading: 'Lexend, serif',
+  body: '"DM Sans", sans-serif',
+  heading: '"BR Firma Bold", serif',
 };
 
 export default fonts;
